4befc4c08c130a574d8f37697a7f3323ff7574b9
huoshiqiang <15732028632@163.com>
2018-03-31 15:44:06 +0800

    画布功能未实现

--- testtest/pages/index/index.js
+++ testtest/pages/index/index.js
@@ -5,14 +5,79 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    picArr: [
+        '../../resources/img/1.jpg',
+        '../../resources/img/2.jpg',
+        '../../resources/img/3.jpg',
+        '../../resources/img/4.jpg',
+    ],
+    viewPic: {
+        url: '',
+        w: 0,
+        h: 0
+    },
+    canvasShow:false,
+    windowW: 375,
+    windowH: 993
+  },
+
+  goThere: function(){
+    wx.openLocation({
+        latitude: 0,
+        longitude: 0,
+    })
+  },
+
+  makeCall: function(){
+    wx.makePhoneCall({
+        phoneNumber: '15633398231',
+    })
+  },
+
+  closeCanvas: function(){
+    this.setData({
+      canvasShow: false
+    })
+  },
+
+  previewImage: function(e){
+      var url = e.currentTarget.dataset.url
+      wx.getImageInfo({
+          src: url,
+          success:(res)=>{
+            var viewPic = {
+                url: url,
+                wh: res.height/res.width,
+            }       
+            this.setData({
+                viewPic: viewPic
+            })
+            var w = this.data.windowW
+            var h = this.data.windowW * this.data.viewPic.wh
+            var top = this.data.windowH / 2 - (h)/2
+            wx.navigateTo({
+              url: `/pages/detail/detail?url=${url}&wh=${this.data.viewPic.wh}`
+            })
+          }
+      })
+  },
+
+  initSys:function(){
+    wx.getSystemInfo({
+      success: (res) =>{
+        this.setData({
+          windowW: res.windowWidth,
+          windowH: res.windowHeight
+        })
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.initSys()
   },
 
   /**
